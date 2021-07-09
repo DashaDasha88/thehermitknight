b2be07e3bfe1f8576c6255f0064b49492bd30690
Dasha <dasha.kotova.mail@gmail.com>
2021-07-09 17:00:05 -0400

    render basic components in the app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from './routes/Home';
 
 const App = () => {
   return (
-    <div>
-      O hai
+    <div className="container">
+      <Router>
+        <Switch>
+
+          <Route exact path="/" component={Home} />
+
+        </Switch>
+      </Router>
     </div>
   )
 }
